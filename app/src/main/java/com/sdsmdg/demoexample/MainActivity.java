27fc8703fa33333256821b4f511995405de5dc47
Mujahid Khan <Mujahid.Khan@hi5host.com>
2020-06-25 20:49:07 +0500

    Rtl issue  fixed
    Rtl issue  fixed

--- app/src/main/java/com/sdsmdg/demoexample/MainActivity.java
+++ app/src/main/java/com/sdsmdg/demoexample/MainActivity.java
@@ -1,5 +1,6 @@
 package com.sdsmdg.demoexample;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
@@ -15,37 +16,41 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        //getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+            getWindow().getDecorView().setLayoutDirection(View.LAYOUT_DIRECTION_RTL);
+        }
     }
 
     public void showSuccessToast(View view) {
-        TastyToast.makeText(getApplicationContext(), "Download Successful !", TastyToast.LENGTH_LONG,
+        TastyToast.makeText(getApplicationContext(), "اردو کا انگریزی میں ترجمہ کریں", TastyToast.LENGTH_SHORT,
                 TastyToast.SUCCESS);
     }
 
     public void showWarningToast(View view) {
-        TastyToast.makeText(getApplicationContext(), "Are you sure ?", TastyToast.LENGTH_LONG,
+        TastyToast.makeText(getApplicationContext(), "Are you sure ?", TastyToast.LENGTH_SHORT,
                 TastyToast.WARNING);
     }
 
     public void showErrorToast(View view) {
-        TastyToast.makeText(getApplicationContext(), "Downloading failed ! Try again later ", TastyToast.LENGTH_LONG,
+        TastyToast.makeText(getApplicationContext(), "Downloading failed ! Try again later ", TastyToast.LENGTH_SHORT,
                 TastyToast.ERROR);
     }
+
     public void showInfoToast(View view) {
-        TastyToast.makeText(getApplicationContext(), "Searching for username : 'Rahul' ", TastyToast.LENGTH_LONG,
+        TastyToast.makeText(getApplicationContext(), "Searching for username : 'Rahul' ", TastyToast.LENGTH_SHORT,
                 TastyToast.INFO);
     }
 
     public void showDefaultToast(View view) {
-        TastyToast.makeText(getApplicationContext(), "This is Default Toast", TastyToast.LENGTH_LONG,
+        TastyToast.makeText(getApplicationContext(), "This is Default Toast", TastyToast.LENGTH_SHORT,
                 TastyToast.DEFAULT);
     }
 
 
     public void showConfusingToast(View view) {
-        TastyToast.makeText(getApplicationContext(), "I don't Know !", TastyToast.LENGTH_LONG,
+        TastyToast.makeText(getApplicationContext(), "I don't Know !", TastyToast.LENGTH_SHORT,
                 TastyToast.CONFUSING);
     }
 }
