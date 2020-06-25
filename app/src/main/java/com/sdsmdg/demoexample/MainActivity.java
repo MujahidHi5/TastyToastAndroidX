0d5a7e47e767df3b4ef9a2a4910cda5dcd5998b6
Mujahid Khan <Mujahid.Khan@hi5host.com>
2020-06-25 17:04:31 +0500

    Fixed
    Fixed

--- app/src/main/java/com/sdsmdg/demoexample/MainActivity.java
+++ app/src/main/java/com/sdsmdg/demoexample/MainActivity.java
@@ -1,9 +1,10 @@
 package com.sdsmdg.demoexample;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.WindowManager;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.sdsmdg.tastytoast.TastyToast;
 
@@ -15,7 +16,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-
         setContentView(R.layout.activity_main);
     }
 
